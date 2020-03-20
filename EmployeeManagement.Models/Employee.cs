2b180603c0e2d60f31b9bed659d33f442e5d8491
Sevris Michael <msevris@hotmail.com>
2020-03-21 00:44:18 +0200

    images

--- EmployeeManagement.Models/Employee.cs
+++ EmployeeManagement.Models/Employee.cs
@@ -12,6 +12,9 @@ namespace EmployeeManagement.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BarCode { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
         public Department Department { get; set; }
